52cd665e13cb116f6cd99b0ad8c2a17b38537bfc
m n <116815127+0xmn1@users.noreply.github.com>
2023-01-06 14:02:52 +1100

    Updates

---
+++ precache-manifest.4566856c80a2f00f42d38320db797ceb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5004a3b30df8a5766055",
+    "url": "/static/css/main.aaac16ad.chunk.css"
+  },
+  {
+    "revision": "5004a3b30df8a5766055",
+    "url": "/static/js/main.5004a3b3.chunk.js"
+  },
+  {
+    "revision": "3d6cb3ca4cc44414fdd8",
+    "url": "/static/js/1.3d6cb3ca.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "04665b65cd83025a7667f8728a381fd0",
+    "url": "/index.html"
+  }
+];
